3c056b3c58c0b1e31c401efb954f314104542935
nchagnet <nchagnet@gmail.com>
2017-03-05 22:52:34 +0100

    the program functions when viewed 2D, 3D display remains to be done

--- main_3d.py
+++ main_3d.py
@@ -3,51 +3,53 @@
 #################### Bibliothèques ###############
 
 import numpy as np
-import matplotlib
-# matplotlib.use("Agg")
 import matplotlib.pyplot as plt
-# plt.rcParams['animation.ffmpeg_path'] = "C:\\ffmpeg\\bin\\ffmpeg.exe"
 import matplotlib.animation as animation
 from copy import deepcopy
 from time import time
 
+#permet de recuperer les overflow
 import warnings
 warnings.filterwarnings("error")
-
-# from multiprocessing import Pool
 
 print('Librairies importees')
 
 ################### Parametres ###################
-#Parametres spatiaux
-longueur_cuve = 100 #axe x
-largeur_cuve = 40 #axe y
-epaisseur_cuve = 4 #axe z
-N = 15
 
-viscosite = 0.01
+#Parametres spatiaux
+longueur_cuve = 60 #axe x
+largeur_cuve = 40 #axe y
+epaisseur_cuve = 1 #axe z
+N = 19
+
+#parametres du fluide
+viscosite = 0.005
 tau = 3 * viscosite + 0.5
 omega = 1. / tau
 Re = 1 #Nombre de Reynolds
-#Position cylindre
-xc = 5
+
+#Position barriere
+xc = 20
 yc = largeur_cuve // 2
 zc = epaisseur_cuve // 2
-rc = 1
+rc = 5
+
+hauteur_barriere = 15
+largeur_barriere = 5
+
 #Vitesse pour le flow
-#calcul recurrents
-u0 = 0.01
+u0 = 0.1
 u02 = u0 ** 2
-poids = np.array([1. / 3] + [1. / 18] * 6 + [1. / 36] * 12) #poids de D3Q19
 cs2 = 1 #paramètre de vitesse de la maille
 # nu = cs2 * (tau + 0.5)
-nu = 1
-nu2 = 1 / 3
+# nu = 1 / np.sqrt(3)
+nu = 1.
+nu2 = nu ** 2
 
-autres = [0, 2, 1, 4, 3, 6, 5, 10, 9, 8, 7, 14, 13, 12, 11, 18, 17, 16, 15]
-
+#Parametres du LBM
+poids = np.array([1. / 3] + [1. / 18] * 6 + [1. / 36] * 12) #poids de D3Q19
 vitesses = np.array([
-	[0, 0, 0], #Rest -> 0
+	[0, 0, 0], #Repos -> 0
 	[0, 0, 1], #z ->1,
 	[0, 0, -1], #-z ->2
 	[0, 1, 0], #+y -> 3
@@ -68,30 +70,28 @@ vitesses = np.array([
 	[0, -1, -1], #-y-z -> 18
 ])
 #vecteurs vitesse vi
-#Les vecteurs sont organises telle que la somme de leurs coordonnees
-#donne la position dans le tableau de leur oppose
+#Permet de faire correspondre les vitesses en rebond
+autres = [0, 2, 1, 4, 3, 6, 5, 10, 9, 8, 7, 14, 13, 12, 11, 18, 17, 16, 15]
 
 F = np.array([0, 0, 0]) #Force de gravite
+#Nulle pour l'instant, tant que ça marche pas
 
-
-# def zoneCylindre(x, y, z):
-# 	""" Retourne True si on est dans la zone du cylindre """
-# 	global zc, yx, xc, rc
-# 	d = (z - zc) ** 2 + (y - yc) ** 2 - rc ** 2
-# 	return d >= 0 and x <= xc
-
-def zoneCylindre(x, y, z):
+#Definit où est la barriere
+def zoneBarriere(x, y, z):
 	""" Retourne True si on est dans la zone du cylindre """
 	global zc, yx, xc, rc
 	d = (x - xc) ** 2 + (y - yc) ** 2 + (z - zc) ** 2 - rc ** 2
 	return d <= 0
+	
 
+#On l'utilise pas à cause du y pas placé au bon endroit dans f
 def diff(u, i, j):
 	"""Retourne duidj - dujdi"""
 	duidj = (np.roll(u[i], -1, axis = j) - np.roll(u[i], 1, axis = j))
 	dujdi = (np.roll(u[j], -1, axis = i) - np.roll(u[j], 1, axis = i))
 	return duidj - dujdi
 
+#marche pas pour la raison sus-mentionnee
 # def calculVorticite(u):
 # 	""" Autre calcul de vorticite """
 # 	terme_x = diff(u, 2, 0)
@@ -100,9 +100,9 @@ def diff(u, i, j):
 # 	resultat = np.array([terme_y, terme_y, terme_z])
 # 	return resultat
 
-def calculVorticite(u):
-	""" Autre calcul de vorticite """
-	ux, uy, uz = u
+#calcul de la vorticite
+def calculVorticite(ux, uy, uz):
+	""" Autre calcul de vorticite, on est sur qu'il est bon """
 	duzdy = np.roll(uz, -1, axis = 0) - np.roll(uz, 1, axis = 0)
 	duydz = np.roll(uy, -1, axis = 2) - np.roll(uy, 1, axis = 2)
 	terme_x = duzdy - duydz
@@ -119,51 +119,35 @@ def calculVorticite(u):
 	resultat = np.array([terme_y, terme_y, terme_z])
 	return resultat
 
-def propage_un(i, f):
-	global vitesses
-	""" Sert à propager une direction """
-
-	dx, dy, dz = vitesses[i]
-	#on fait la propagation
-	f[i] = np.roll(f[i], dy, axis = 0)
-	f[i] = np.roll(f[i], dx, axis = 1)
-	f[i] = np.roll(f[i], dz, axis = 2)
-
-	return f[i]
-
-def propage_un_parralel(args):
-	return propage_un(*args)
-
-# propage_un_parralel.parallel = parallel_attribute(propage_un_parralel)
-
+#Fonction de propagation dans toutes les directions
 def propagation():
-	""" On propage les densites selon les directions """
-	global f
-	#Valuer macroscopique
+	global f, vitesses
+	
+	#Calcul de rho
 	rho = np.sum(f, axis = 0)
+	
 	#Propagation
 	for i in range(N):
-		f[i] = propage_un(i, f)
-	# f = propage_un_parralel.parallel(N)
-	# from multiprocessing import Pool
-	# pool = Pool()
-	# results = [pool.apply(propage_un, args = (i, f)) for i in range(N)]
-	# f = np.array(results)
-	# pool.close()
-	# pool.join()
 
+		#on fait la propagation axe par axe
+		dx, dy, dz = vitesses[i]
+		f[i] = np.roll(f[i], dy, axis = 0)
+		f[i] = np.roll(f[i], dx, axis = 1)
+		f[i] = np.roll(f[i], dz, axis = 2)
+
+	#on garde f en copie pour l'operation suivante
 	f_copy = deepcopy(f)
 	#collision avec les obstacles
 	for i in range(1, N):
 		#On recupere la direction "opposee"
 		autre = autres[i]
+		#On echange les directions opposees
 		f[i][barrier[i]] = f_copy[autre][barrier[0]]
 
 
-
+#Cette fonction gere le terme de collisions
 def collisions():
-	""" Cette fonction gere le terme de collisions """
-	global f, ux, uy, uz, rho, u0, u02, omega
+	global f, u0, u02, omega
 
 	#Valeurs macroscopiques
 	rho = np.sum(f, axis = 0)
@@ -179,58 +163,53 @@ def collisions():
 	ux = ux / rho
 	uy = uy / rho
 	uz = uz / rho
-	terme1 = 1 - 1.5 * (ux ** 2 + uy ** 2 + uz ** 2)
+
+	#on evite de recalculer N fois ce terme constant
+	terme1 = 1 - 1.5 * (ux ** 2 + uy ** 2 + uz ** 2) / nu2
 
 	#on applique la formule BGK
 	for i in range(N):
 		ps = vitesses[i, 0] * ux + vitesses[i, 1] * uy + vitesses[i, 2] * uz
 		terme2 = 3 * ps / nu
 		terme3 = 4.5 * np.power(ps, 2) / nu2
-		f[i] = (1 - omega) * f[i] + omega * poids[i] * rho * (terme1 + terme2 + terme3)
+		# f[i] = (1 - omega) * f[i] + omega * poids[i] * rho * (terme1 + terme2 + terme3)
+		#on capture les erreurs d'overflow
 		try:
 			f[i] = (1 - omega) * f[i] + omega * poids[i] * rho * (terme1 + terme2 + terme3)
 		except RuntimeWarning:
-			print('hihi')
+			print('Erreur d\'overflow')
 	
 	#on ajoute le champ de force
 	for i in range(N):
 		ps = np.dot(vitesses[i], F)
-		f[i] += poids[i] * ps / nu2
+		f[i] += poids[i] * ps / nu
 	
 	#On force le flow en entree
+
+	#Calcul du terme recurrent
 	terme1 = 1 - 1.5 * u02 / nu2
 
+	#on ne s'occupe que des directions concernees par le flow horizontal
 	for i in [1, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]:
 		ps = vitesses[i, 0] * u0
 		terme2 = 3 * ps / nu
 		terme3 = 4.5 * np.abs(vitesses[i, 0]) * u02 / nu2
 
-		f[i, :, 0] = poids[i] * (terme1 + terme2 + terme3)
-
-	# for i in [1, 3, 4, 5, 7, 8]:
-	# 	ps = vitesses[i, 0] * u0
-	# 	terme2 = 3 * ps / nu
-	# 	terme3 = 4.5 * np.abs(vitesses[i, 0]) * u02 / nu2
-
-	# 	f[i, 0, :] = poids[i] * (terme1 + terme2 + terme3)
-	# 	f[i, largeur_cuve - 1, :] = poids[i] * (terme1 + terme2 + terme3)
+		f[i, :, 0, :] = poids[i] * (terme1 + terme2 + terme3)
 
 
 if __name__ == '__main__':
-	########On definit la barriere
+	######## On definit la barriere ###########
 	barrier = np.zeros((N, largeur_cuve, longueur_cuve, epaisseur_cuve), dtype = bool)
-	#barriere horizontale
-	# barrier[0, 0, :] = True
-	# barrier[0, largeur_cuve - 1, :] = True
-	# on construit la barriere cylindrique
-	for x in range(longueur_cuve):
-		for y in range(largeur_cuve):
-			for z in range(epaisseur_cuve):
-				if zoneCylindre(x, y, z):
-					barrier[0, y, x, z] = True
-	# barrier[0, (largeur_cuve // 2) - taille_barriere:(largeur_cuve // 2) + taille_barriere, largeur_cuve // 2] = True
-	# barrier[0, :hauteur_barriere, :largeur_barriere] = True
-	# barrier[0, largeur_cuve - hauteur_barriere:, :largeur_barriere] = True
+
+	# for x in range(longueur_cuve):
+	# 	for y in range(largeur_cuve):
+	# 		for z in range(epaisseur_cuve):
+	# 			if zoneBarriere(x, y, z):
+	# 				barrier[0, y, x, z] = True
+
+	barrier[0, :hauteur_barriere, :largeur_barriere, :] = True
+	barrier[0, largeur_cuve - hauteur_barriere:, :largeur_barriere, :] = True
 
 	#Ces booleens servent a gerer les collisions
 	for i in range(N):
@@ -240,9 +219,10 @@ if __name__ == '__main__':
 		barrier[i] = np.roll(barrier[i], dx, axis = 1)
 		barrier[i] = np.roll(barrier[i], dz, axis = 2)
 
-	##########On initialise avec un flux constant
+	########## On initialise avec un flux constant ################
 	f = np.ones((N, largeur_cuve, longueur_cuve, epaisseur_cuve))
 	terme1 = 1 - 1.5 * u02 / nu2
+	
 	for i in range(N):
 		#produit scalaire de la vitesse forcee avec la vitesse horizontale
 		ps = vitesses[i, 0] * u0
@@ -250,7 +230,8 @@ if __name__ == '__main__':
 		terme3 = 4.5 * np.abs(vitesses[i, 0]) * u02 / nu2
 
 		f[i] = poids[i] * (terme1 + terme2 + terme3)
-	#Valeurs macroscopiques
+	
+	#Calcul des valeurs macroscopiques
 	rho = np.sum(f, axis = 0) #densite
 	#vitesses
 	ux = np.zeros((largeur_cuve, longueur_cuve, epaisseur_cuve))
@@ -270,30 +251,22 @@ if __name__ == '__main__':
 	########## Affichage #############
 	##################################
 	print('Affichage')
-	# fig, axes = plt.subplots(1, 2, figsize = (16, 6))
-	fig, axes = plt.subplots(1, 1)
-	# ax1, ax2 = axes
-	ax1 = axes
+	fig, ax1 = plt.subplots(1, 1)
 	
-	w = calculVorticite(np.array([ux, uy, uz]))
+	w = calculVorticite(ux, uy, uz)
 	# Affichage vorticite
 	fluidImage = ax1.imshow(
 		w[2, :, :, zc], 
-		origin='lower', 
+		origin = 'lower', 
 		norm = plt.Normalize(-.1,.1), 
 		cmap = plt.get_cmap('jet'), 
 		interpolation = 'none'
 	)
 
 	#affichage des barrieres
-	bImageArray = np.zeros((largeur_cuve, longueur_cuve, 4), np.uint8)	# an RGBA image
-	bImageArray[barrier[0, :, :, zc], 3] = 255								# set alpha=255 only at barrier sites
-	barrierImage = ax1.imshow(bImageArray, origin = 'lower', interpolation='none')
-
-	#Affichage du flow
-	# X = np.linspace(0, longueur_cuve, longueur_cuve)
-	# Y = np.linspace(0, largeur_cuve, largeur_cuve)
-	# strm = ax2.streamplot(X, Y, ux, uy, linewidth = 2, cmap = plt.cm.autumn)
+	bImageArray = np.zeros((largeur_cuve, longueur_cuve, 4), np.uint8)
+	bImageArray[barrier[0, :, :, zc], 3] = 255
+	barrierImage = ax1.imshow(bImageArray, origin = 'lower', interpolation = 'none')
 
 	#fonction d'animation
 	def nextFrame(k, *args):
@@ -302,20 +275,35 @@ if __name__ == '__main__':
 		for step in range(30): #A ajuster pour que ça soit bien
 			propagation()
 			collisions()
-		w = calculVorticite(np.array([ux, uy, uz]))
+
+		#Calcul des valeurs macroscopiques
+		rho = np.sum(f, axis = 0) #densite
+		#vitesses
+		ux = np.zeros((largeur_cuve, longueur_cuve, epaisseur_cuve))
+		uy = np.zeros((largeur_cuve, longueur_cuve, epaisseur_cuve))
+		uz = np.zeros((largeur_cuve, longueur_cuve, epaisseur_cuve))
+
+		for i in range(N):
+			ux += vitesses[i, 0] * f[i]
+			uy += vitesses[i, 1] * f[i]
+			uz += vitesses[i, 2] * f[i]
+
+		ux = ux / rho
+		uy = uy / rho
+		uz = uz / rho
+
+		w = calculVorticite(ux, uy, uz)
+
+		#on modifie l'image
 		fluidImage.set_array(w[2, :, :, zc])
 
-		# if k > 0:
-		# 	u0 = 0
+		#on annule la vitesse
+		#C'est comme si l'obstacle s'arretait de bouger
+		if k > 0:
+			u0 = 0
 
-		# ax2.cla()
-		# strm = ax2.streamplot(X, Y, ux, uy, linewidth = 2, cmap = plt.cm.autumn)
-		# plt.draw()
 		return (fluidImage, barrierImage)
 
-	# Set up formatting for the movie files
-	# Writer = animation.writers['ffmpeg']
-	# writer = Writer(fps = 15, metadata = dict(artist = 'Me'), bitrate = 1800)
 
 	#animation
 	ani = animation.FuncAnimation(
@@ -326,5 +314,4 @@ if __name__ == '__main__':
 		interval = 1, 
 		blit = True)
 
-	# ani.save('test.mp4', writer = writer)
 	plt.show()
